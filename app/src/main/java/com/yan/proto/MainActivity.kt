39b89c34e778bc0aa4ccb2a46abd6483a9bf4a06
yingyanwu <1179858356@qq.com>
2023-02-26 22:16:08 +0800

    添加preference dataStore的实现

--- app/src/main/java/com/yan/proto/MainActivity.kt
+++ app/src/main/java/com/yan/proto/MainActivity.kt
@@ -3,17 +3,15 @@ package com.yan.proto
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.datastore.preferences.core.edit
 import androidx.lifecycle.lifecycleScope
 import com.yan.proto.ui.theme.ProtoDataStoreTheme
 import kotlinx.coroutines.cancel
@@ -30,17 +28,31 @@ class MainActivity : ComponentActivity() {
             }
     }
 
+    private val animalNameFlow: Flow<String> by lazy {
+        dataStore.data
+            .map { preferences ->
+                // No type safety.
+                preferences[EXAMPLE_COUNTER] ?: "Ant"
+            }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ProtoDataStoreTheme {
                 val counter by exampleCounterFlow.collectAsState(initial = 0)
+                val animalName by animalNameFlow.collectAsState(initial = "Ant")
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android $counter", increment = { incrementCounter() })
+                    Column {
+                        UsingProto("Android $counter", increment = { incrementCounter() })
+                        UsingPreference(name = animalName, setName = {
+                            updatingAnimalName(it)
+                        })
+                    }
                 }
             }
         }
@@ -56,6 +68,14 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    private fun updatingAnimalName(name: String) {
+        lifecycleScope.launch {
+            dataStore.edit { settings ->
+                settings[EXAMPLE_COUNTER] = name
+            }
+        }
+    }
+
     override fun onDestroy() {
         super.onDestroy()
         lifecycleScope.cancel()
@@ -63,7 +83,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, increment: () -> Unit = {}) {
+fun UsingProto(name: String, increment: () -> Unit = {}) {
     Row {
         Text(text = "Hello $name!")
         Button(onClick = { increment() }) {
@@ -72,10 +92,27 @@ fun Greeting(name: String, increment: () -> Unit = {}) {
     }
 }
 
+@Composable
+fun UsingPreference(name: String, setName: (String) -> Unit = {}) {
+    Row {
+        var value by remember {
+            mutableStateOf("")
+        }
+        LaunchedEffect(Unit) {
+            value = name
+        }
+        TextField(value = value, onValueChange = { value = it }, modifier = Modifier.weight(1f))
+        Text(text = name, modifier = Modifier.weight(1f), color = Color.Green)
+        Button(onClick = { setName(value) }, modifier = Modifier.weight(1f)) {
+            Text(text = "Updating Name")
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ProtoDataStoreTheme {
-        Greeting("Android")
+        UsingProto("Android")
     }
 }
---
+++ app/src/main/java/com/yan/proto/PreferencesDataStoreUtil.kt
@@ -0,0 +1,11 @@
+package com.yan.proto
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "animal")
+
+val EXAMPLE_COUNTER = stringPreferencesKey("animal_name")
